98e80c10ceb3da0ad5dd1c589bc8622571d9eae1
Roxane <rjworks17100@gmail.com>
2024-11-25 14:38:36 +0100

    test for userControllers

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 
 const app = require("../src/app");
 
+
 describe("GET /api/movies", () => {
   it("should return all movies", async () => {
     const response = await request(app).get("/api/movies");
